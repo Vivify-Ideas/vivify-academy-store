b401d41e6d00bce61f6c18a8e87a3bec42b6b874
Nenad Vujicic <nndprogramer@gmail.com>
2018-08-03 09:35:00 +0200

    Zadatak 5

--- src/views/AppProducts.vue
+++ src/views/AppProducts.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
-    Products
+    <h1>Products</h1>
+    <hr/>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td>{{ product.name }}</td>
+          <td>{{ product.quantity }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-  export default {
+  import { productService } from '../utils/ProductService'
 
+  export default {
+    data() {
+      return {
+        products: productService.list()
+      }
+    }
   }
 </script>
